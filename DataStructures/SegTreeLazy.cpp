c5731f51b5784865d8afe45d83ed30dc92a2a7ca
Shaswat Tripathi <shaswat749@gmail.com>
2024-07-30 14:59:46 +0530

    lazy  seg tree

---
+++ DataStructures/SegTreeLazy.cpp
@@ -0,0 +1,33 @@
+vi t(4*N) , lazy(4*N);
+void push(int v) {
+    t[v*2] += lazy[v];
+    lazy[v*2] += lazy[v];
+    t[v*2+1] += lazy[v];
+    lazy[v*2+1] += lazy[v];
+    lazy[v] = 0;
+}
+
+void update(int v, int tl, int tr, int l, int r, int addend) {
+    if (l > r)
+        return;
+    if (l == tl && tr == r) {
+        t[v] += addend;
+        lazy[v] += addend;
+    } else {
+        push(v);
+        int tm = (tl + tr) / 2;
+        update(v*2, tl, tm, l, min(r, tm), addend);
+        update(v*2+1, tm+1, tr, max(l, tm+1), r, addend);
+        t[v] = min(t[v*2], t[v*2+1]);
+    }
+}
+int query(int v, int tl, int tr, int l, int r) {
+    if (l > r)
+        return inf;
+    if (l <= tl && tr <= r)
+        return t[v];
+    push(v);
+    int tm = (tl + tr) / 2;
+    return min(query(v*2, tl, tm, l, min(r, tm)),
+            query(v*2+1, tm+1, tr, max(l, tm+1), r));
+}
