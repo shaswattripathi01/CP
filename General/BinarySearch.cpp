ab9b167d1fa5eb80048d69cf7593d107295ccb55
Shaswat Tripathi <shaswat749@gmail.com>
2024-07-30 14:45:57 +0530

    general binary search

---
+++ General/BinarySearch.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main() {
+  int s = 1, e = 1000, m , p=-1;
+  
+  while(s <= e){
+    m = (s+e)/2;
+    if(1){
+      //
+      p = m;
+      e = m - 1;
+    }else {
+      s = m + 1;
+    }
+  }
+  
+  if(p){}
+  
+}
+
